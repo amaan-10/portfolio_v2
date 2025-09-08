7ff6ab56ac7fa283ae2a94f905caf4ff152950da
Amaan <amaanshaikh.gg@gmail.com>
2025-09-08 23:36:06 +0530

    update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import Header from "@/components/header";
 import Projects from "@/components/projects/projects";
 import Footer from "@/components/footer";
-import ContinuousAnimations from "@/components/continuous-animations";
+import BGAnimations from "@/components/bg-animations";
 import ScrollProgress from "@/components/scroll-progress";
 import HandwritingAnimation from "@/components/loading";
 
@@ -28,13 +28,13 @@ export default function Home() {
       ) : (
         <motion.div
           key="main"
-          className="min-h-screen bg-white text-black overflow-x"
+          className="min-h-screen overflow-x text-white bg-black"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
           <ScrollProgress />
-          <ContinuousAnimations />
+          <BGAnimations blurValue={10} />
           <Header />
           <main>
             <Projects showAll />
